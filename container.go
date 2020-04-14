2800fbc884d16c26e0d0a4c9a79009875b1c23cd
fairyhunter13 <fairyhunter13@gmail.com>
2020-04-15 01:10:54 +0700

    - Separate publish args :art: - Adding publish function :sparkles:

--- container.go
+++ container.go
@@ -28,23 +28,42 @@ func NewContainer(conn amqpwrapper.IConnectionManager) (res *Container, err erro
 	return
 }
 
+// Publish publishes the message to the default exchange with the default topic.
+func (c *Container) Publish(exchange, topic string, args OtherPublishArgs) (err error) {
+	c.setDefaultExchange()
+	if exchange == "" {
+		c.mutex.RLock()
+		exchange = c.globalExchange.Name
+		c.mutex.RUnlock()
+	}
+	if topic == "" {
+		topic = DefaultKey
+	}
+	pmArgs := PublishArgs{
+		Exchange:         exchange,
+		Key:              topic,
+		OtherPublishArgs: args,
+	}
+	err = c.publisherManager.Publish(pmArgs)
+	return
+}
+
 // SetExchangeName sets the exchange name of all publishers and consumers
 func (c *Container) SetExchangeName(name string) *Container {
-	c.setExchangeName(name)
+	c.setDefaultExchange()
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if name != "" {
+		c.globalExchange.Name = name
+	}
+	c.topology.AddExchangeDeclare(*c.globalExchange)
 	return c
 }
 
-func (c *Container) setExchangeName(name string) {
-	c.mutex.RLock()
+func (c *Container) setDefaultExchange() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
 	if c.globalExchange == nil {
-		c.mutex.RUnlock()
-		c.mutex.Lock()
 		c.globalExchange = new(ExchangeDeclareArgs).Default()
-		c.mutex.Unlock()
-		c.mutex.RLock()
-		c.topology.AddExchangeDeclare(*c.globalExchange)
-		c.mutex.RUnlock()
-	} else {
-		c.mutex.RUnlock()
 	}
 }
