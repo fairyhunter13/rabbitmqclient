ff95a626e1a8fd3f6bb2fc8f7961de4483ea0c4c
fairyhunter13 <fairyhunter13@gmail.com>
2020-04-15 01:10:54 +0700

    - Separate publish args :art: - Adding publish function :sparkles:

--- common_args.go
+++ common_args.go
@@ -4,8 +4,13 @@ import "github.com/streadway/amqp"
 
 // PublishArgs specifies the arguments to publish function
 type PublishArgs struct {
-	Exchange  string
-	Key       string
+	Exchange string
+	Key      string
+	OtherPublishArgs
+}
+
+// OtherPublishArgs specifies the others arg for the publish function.
+type OtherPublishArgs struct {
 	Mandatory bool
 	Immediate bool
 	Msg       amqp.Publishing
