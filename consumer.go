23d5738b09edd18c354237cda02fd18e39046745
fairyhunter13 <fairyhunter13@gmail.com>
2020-05-29 14:35:34 +0700

    - Finishing on consumer :sparkles:

--- consumer.go
+++ consumer.go
@@ -4,16 +4,30 @@ import "sync"
 
 // Consumer defines the behavior class for the consumer
 type Consumer struct {
+	container *Container
+	*saver
 	mutex *sync.RWMutex
 	// mutex protects the following fields
 	declare *QueueDeclare
 	bind    *QueueBind
 }
 
-func newConsumer() *Consumer {
+func newConsumer(container *Container) *Consumer {
 	return &Consumer{
-		mutex:   new(sync.RWMutex),
-		declare: new(QueueDeclare).Default(),
-		bind:    new(QueueBind).Default(),
+		container: container,
+		mutex:     new(sync.RWMutex),
+		declare:   new(QueueDeclare).Default(),
+		bind:      new(QueueBind).Default(),
+		saver:     newSaver(),
 	}
+}
+
+// Save saves the consumer state.
+func (c *Consumer) Save() *Consumer {
+	if !c.isSaved() {
+		c.container.AddQueueDeclare(*c.declare)
+		c.container.AddQueueBind(*c.bind)
+		c.save()
+	}
+	return c
 }
