63d9e91d05854600516938a01d99c41fee7d3c49
fairyhunter13 <fairyhunter13@gmail.com>
2020-06-03 14:44:14 +0700

    - Adding doc :pencil:

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rabbitmqclient
+[![Coverage Status](https://coveralls.io/repos/github/fairyhunter13/rabbitmqclient/badge.svg?branch=master)](https://coveralls.io/github/fairyhunter13/rabbitmqclient?branch=master)
+[![CircleCI](https://circleci.com/gh/fairyhunter13/amqpwrapper.svg?style=svg)](https://circleci.com/gh/fairyhunter13/amqpwrapper)
+[![Go Report Card](https://goreportcard.com/badge/github.com/fairyhunter13/rabbitmqclient)](https://goreportcard.com/report/github.com/fairyhunter13/rabbitmqclient)
+
+
 The rabbitmq client built using fairyhunter13/amqpwrapper. This client only contains default topology, for more detailed topology, use fairyhunter13/amqpwrapper.
--- doc.go
+++ doc.go
@@ -1,7 +1,9 @@
 /*
-Package rabbitmqclient is a package for managing publishers and consumers inside RabbitMQ.
-This package is built on top of github.com/fairyhunter13/rabbitmqclient.
-This package supports auto-reconnection for RabbitMQ connection.
-This package can also manage the topology of queue network's architecture inside RabbitMQ.
-The purpose of this package is to simplify the process of publishing and consuming for the RabbitMQ client.
-*/package rabbitmqclient
+	Package rabbitmqclient is a package for managing publishers and consumers inside RabbitMQ.
+
+	This package is built on top of github.com/fairyhunter13/rabbitmqclient.
+	This package supports auto-reconnection for RabbitMQ connection.
+	This package can also manage the topology of queue network's architecture inside RabbitMQ.
+	The purpose of this package is to simplify the process of publishing and consuming for the RabbitMQ client.
+*/
+package rabbitmqclient
